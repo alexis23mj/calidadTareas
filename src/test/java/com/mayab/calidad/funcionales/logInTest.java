fbc382fd01332c296448762c83e733878bf4aef4
alexis23mj <angel_23mj@hotmail.com>
2019-10-24 20:43:29 -0500

    LogIn actualizado

--- src/test/java/com/mayab/calidad/funcionales/logInTest.java
+++ src/test/java/com/mayab/calidad/funcionales/logInTest.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.Assert.assertThat;
 
 /**
  *
@@ -34,17 +35,91 @@ public class logInTest {
     //driver.manage().timeouts().implicitlyWait(30, TimeUnit
     
   }
+    public void pause(long mills){
+      try{
+          Thread.sleep(1000);
+      }catch(Exception e){
+          e.printStackTrace();
+      }
+  }
+  
   @Test
   public void testUntitledTestCase() throws Exception {
       driver.get(URL);
       
-      WebElement element= driver.findElement(By.name("#loginbox"));
+      WebElement element= driver.findElement(By.id("user_id"));
       
-      element.sendKeys("piñata");
+      element.sendKeys("00330796");
+      
+      element= driver.findElement(By.id("password"));
+      
+      element.sendKeys("00330796");
+      
+      element= driver.findElement(By.id("entry-login"));
       
       element.submit();
       
-      pause(5000);
+      element= driver.findElement(By.xpath("/html/body/div[3]/div[2]/a"));
+      String name= element.getText();
+      
+      element.click();
+      
+      
+      pause(50000);
+      
+      
+      assertEquals("Alexis Morales Diaz",name);
+      
+      driver.close();
+    
+  }
+    @Test
+  public void testError() throws Exception {
+      driver.get(URL);
+      
+      WebElement element= driver.findElement(By.id("user_id"));
+      
+      element.sendKeys("00330796");
+      
+      element= driver.findElement(By.id("password"));
+      
+      element.sendKeys("30796");
+      
+      element= driver.findElement(By.id("entry-login"));
+      
+      element.submit();
+      
+      element= driver.findElement(By.id("loginErrorMessage"));
+      String error= element.getText();
+      
+      element.click();
+      
+      
+      pause(50000);
+      
+      
+      assertEquals("El nombre de usuario o contraseña que ha introducido no son correctos. Inténtelo de nuevo. Si aún no puede iniciar sesión, comuníquese con su administrador del sistema.",error);
+      
+      driver.close();
+    
+  }
+      @Test
+  public void testEmpty() throws Exception {
+      driver.get(URL);
+      
+      WebElement element= driver.findElement(By.id("entry-login"));
+      
+      element.submit();
+      
+       pause(50000);
+      
+      String error= driver.switchTo().alert().getText();
+      driver.switchTo().alert().accept();
+      
+      
+      
+      
+      assertEquals("Introduzca un nombre de usuario y una contraseña.",error);
       
       driver.close();
     
@@ -54,13 +129,7 @@ public class logInTest {
       driver.quit();
   }
   
-  public void pause(long mills){
-      try{
-          Thread.sleep(1000);
-      }catch(Exception e){
-          e.printStackTrace();
-      }
-  }
+
 
 
     
