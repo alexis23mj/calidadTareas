6cc8c02b79270b8a2ea20a381d40746578a23067
alexis23mj <angel_23mj@hotmail.com>
2019-11-20 12:53:34 -0600

    Update

--- src/test/java/com/mayab/calidad/funcionales/parcialTest.java
+++ src/test/java/com/mayab/calidad/funcionales/parcialTest.java
@@ -15,11 +15,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import static org.junit.Assert.assertThat;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+import org.springframework.core.annotation.Order;
 
 /**
  *
  * @author Angel
  */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class parcialTest {
   private WebDriver driver;
   private String baseUrl;
@@ -37,14 +41,14 @@ public class parcialTest {
   }
     public void pause(long mills){
       try{
-          Thread.sleep(1000);
+          Thread.sleep(mills);
       }catch(Exception e){
           e.printStackTrace();
       }
   }
   
-  /*@Test
-  public void testAdd() throws Exception {
+  @Test
+  public void testAAdd() throws Exception {
       driver.get(URL);
       
       WebElement element= driver.findElement(By.xpath("/html/body/div/div/div[2]/button"));
@@ -67,7 +71,7 @@ public class parcialTest {
       
       element.click();  
       
-     pause(50000);
+     pause(3000);
      
      element = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[4]/div/p"));
       
@@ -77,9 +81,10 @@ public class parcialTest {
       
       
       driver.close();   
-  }*/
-    /*@Test
-  public void testAddError() throws Exception {
+  }
+   
+  @Test
+  public void testBAddError() throws Exception {
       driver.get(URL);
       
      WebElement element= driver.findElement(By.xpath("/html/body/div/div/div[2]/button"));
@@ -102,7 +107,7 @@ public class parcialTest {
       
      element.click();
       
-     pause(50000);
+     pause(5000);
      
      element= driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[5]/div/p"));
      
@@ -112,35 +117,145 @@ public class parcialTest {
       
      driver.close();
     
-  }*/
-     @Test
-  public void tesDelete() throws Exception {
+  }
+  
+  @Test 
+  public void testCDelete() throws Exception {
       driver.get(URL);
       
-      WebElement element = driver.findElement(By.cssSelector("tr:nth-child(1) > td:nth-child(1)"));
+      WebElement element = driver.findElement(By.xpath("/html/body/div/div/div[2]/table/tbody/tr[1]/td[1]"));
       
-      String name = element.getClass().toString();
+      pause(1000);
       
+      String name = element.getText();
+      pause(1000);
       
       element = driver.findElement(By.cssSelector("tr:nth-child(1) .black"));
-      
+      pause(1000);
       
       element.click();
+      pause(1000);
       
       element = driver.findElement(By.cssSelector(".red"));
+      pause(1000);
       
       element.click();
+      pause(1000);
       
       WebElement e2 = driver.findElement(By.cssSelector("tr:nth-child(1) > td:nth-child(1)"));
       
-      String name2 = e2.getClass().toString();
+      pause(3000);
       
-      pause(50000);
+      String name2 = e2.getText();
+      
+      pause(1000);
       
       assertNotEquals(name,name2);
+      pause(1000);
       
       driver.close();
     
+  }
+ @Test
+  public void testDDeleteFail() throws Exception {
+      driver.get(URL);
+      
+      WebElement element = driver.findElement(By.xpath("/html/body/div/div/div[2]/table/tbody/tr[1]/td[1]"));
+      
+      pause(1000);
+      
+      String name = element.getText();
+      pause(1000);
+      
+      element = driver.findElement(By.cssSelector("tr:nth-child(1) .black"));
+      pause(1000);
+      
+      element.click();
+      pause(1000);
+      
+      element = driver.findElement(By.xpath("/html/body/div[2]/div/div[3]/button[2]"));
+      pause(1000);
+      
+      element.click();
+      pause(1000);
+      
+      WebElement e2 = driver.findElement(By.cssSelector("tr:nth-child(1) > td:nth-child(1)"));
+      
+      pause(3000);
+      
+      String name2 = e2.getText();
+      
+      pause(1000);
+      
+      assertEquals(name,name2);
+      pause(1000);
+      
+      driver.close();
+    
+  }    
+      @Test
+  public void testEEdit() throws Exception {
+      driver.get(URL);
+      
+      WebElement element = driver.findElement(By.xpath("/html/body/div/div/div[2]/table/tbody/tr/td[5]/button[1]"));
+      
+      element.click();
+      
+      element = driver.findElement(By.name("email"));
+      
+      pause(5000);
+      
+      element.clear();
+      
+      element.sendKeys("edited@test.com");
+      
+      
+      element = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/button"));
+      
+      element.click();
+      
+      pause(5000);
+      
+      element = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[4]/div/p"));
+      
+      String name2 = element.getText();
+      
+      assertEquals("Successfully updated!",name2);
+      
+      driver.close();
+    
+  }
+    
+      @Test
+  public void testFEditFail() throws Exception {
+      driver.get(URL);
+      
+      WebElement element = driver.findElement(By.xpath("/html/body/div/div/div[2]/table/tbody/tr/td[5]/button[1]"));
+      
+      element.click();
+      
+      element = driver.findElement(By.name("email"));
+      
+      pause(5000);
+      
+      element.clear();
+      
+      element.sendKeys("fail@test");
+      
+      
+      element = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/button"));
+      
+      element.click();
+      
+      pause(5000);
+      
+      element = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[5]/div/p"));
+      
+      String name2 = element.getText();
+      
+      assertEquals("Email must be valid.",name2);
+      
+      driver.close();
   }
     @After
   public void tearDown(){
