1241016ad27f6abd0910c16dad2bfe41b1901ba5
alexis23mj <angel_23mj@hotmail.com>
2019-11-19 19:54:36 -0600

    update

--- src/test/java/com/mayab/calidad/bdd/TestRunner.java
+++ src/test/java/com/mayab/calidad/bdd/TestRunner.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Features;
+package com.mayab.calidad.bdd;
 
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
